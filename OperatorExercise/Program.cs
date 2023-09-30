117fcd0f798cfd760e9c65dfb58b74cddfb0f9ae
sarajadidi <sararivkajadidi@gmail.com>
2023-09-30 15:43:35 -0700

    added a file and began working

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("hello");
+            int a = 17;
+            int b = 4;
+
+            Console.WriteLine()
+
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+            Console.WriteLine("a + b");
         }
     }
 }
